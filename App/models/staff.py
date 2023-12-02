bc8fbce25ce2ee0b74ffd6c8e0be1f2ad63b65ce
Christin120 <98425721+Christin120@users.noreply.github.com>
2023-12-01 20:01:46 -0400

    Update staff.py

--- App/models/staff.py
+++ App/models/staff.py
@@ -7,7 +7,7 @@ class Staff(User):
     staffID = db.Column(db.Integer, primary_key=True)
     departmentCode = db.Column(db.String(10), db.ForeignKey(department.departmentCode), nullable = False)
     firstName = db.Column(db.String(50), nullable = False)
-    firstName = db.Column(db.String(50), nullable = False)
+    lastName = db.Column(db.String(50), nullable = False)
     email = db.Column(db.String(254),nullable = False )
 
     department = relationship('Department', backref='staff_members')
