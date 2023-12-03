d297724b284988fb9326d5f69e6d6d8f48c79df5
alliyah01 <124291804+alliyah01@users.noreply.github.com>
2023-12-03 18:04:09 -0400

    Added Integration Tests

--- App/tests/department.py
+++ App/tests/department.py
@@ -14,3 +14,14 @@ class DepartmentUnitTests(unittest.TestCase):
     def test_department_toJSON(self):
         newDepartment = Department("DCIT", "Department of Computing and Information Technology")
         assert(newDepartment.get_json(), {"Department Code":"DCIT", "Department Name":"Department of Computing and Information Technology"})
+
+"Integration Tests"
+class DepartmentIntegrationTests(unittest.TestCase):
+    def test_create_department(self):
+        department_code = 'DCIT'
+        department_name = 'Department of Computing and Technology'
+        created_department = create_department(department_code, department_name)
+
+        self.assertIsNotNone(created_department)
+        retrieved_department = get_department_by_code(department_code)
+        self.assertEqual(retrieved_department, created_department)
