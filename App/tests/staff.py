6f7501282c7be7487a32760aa6003087ac2c360e
SGajadhar <shaniajgajadhar@gmail.com>
2023-12-03 13:58:19 +0000

    edit tests

--- App/tests/staff.py
+++ App/tests/staff.py
@@ -13,23 +13,22 @@ class StaffUnitTests(unittest.TestCase):
 
     def test_new_staff(self):
         #newUser = User()
-        newStaff = Staff("bob@mail.com", "DCIT", "Bobby", "Smith")
-        assert (newStaff.email, newStaff.departmentCode, newStaff.firstName,  newStaff.lastName) == ("bob@mail.com", "DCIT", "Bobby", "Smith")
+        newStaff = Staff(101, "DCIT", "Bobby", "Smith", "bob@mail.com", "bob", "bobpass")
+        assert (newStaff.staffID, newStaff.departmentCode, newStaff.firstName,  newStaff.lastName, newStaff.email, newStaff.username) == (101, "DCIT", "Bobby", "Smith", "bob@mail.com", "bob")
 
     # pure function no side effects or integrations called
     def test_staff_toJSON(self):
-        newStaff = Staff("bob", "bobpass", "bob@mail.com", "DCIT", "Bobby", "Smith")
-        #staff_json = newStaff.toDict()
+        newStaff = Staff(101, "DCIT", "Bobby", "Smith", "bob@mail.com", "bob", "bobpass")
         self.assertDictEqual(newStaff.get_json(), {"Staff ID":101, "First Name":"Bobby", "Last Name":"Smith", "Email":"bob@mail.com", "Department Code":"DCIT", "Username":"bob"})
     
 
     def test_hashed_password(self):
         password = "pass123"
         hashed = generate_password_hash(password, method='sha256')
-        newStaff = Staff("bob", "bobpass", "bob@mail.com", "Bobby", "Smith")
+        newStaff = Staff(101, "DCIT", "Bobby", "Smith", "bob@mail.com", "bob", "bobpass")
         assert newStaff.password != password
 
     def test_check_password(self):
         password = "pass123"
-        newStaff = Staff("bob", password, "bob@mail.com", "Bobby", "Smith")
+        newStaff = Staff(101, "DCIT", "Bobby", "Smith", "bob@mail.com", "bob", password)
         assert newStaff.check_password(password) 
